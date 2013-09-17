908b668d9948a98698db2fef48ead2a50641ea1c
cfeliciano <c.feliciano2009@gmail.com>
2013-09-17 01:14:49 -0400

    Added a try except in case the user doesn't have the dns.resolver package.

--- mailclient.py
+++ mailclient.py
@@ -8,19 +8,25 @@
 #Dependencies: dnspython -- pip install dnspython
 
 from socket import *
-import sys, dns.resolver
+import sys
 
-smtpPort   = 25
+#import dns.resolver to get the MX server names.
+try:
+    import dns.resolver
+except ImportError:
+    raise SystemExit("Please install dnspython using: pip install dnspython")
 try:
     fromEmail  = sys.argv[1]
     toEmail    = sys.argv[2]
 except:
     raise SystemExit("Usage: mailclient.py <From Email> <To Email>")
 
+smtpPort   = 25
 #Split email to get the domain name
 domain = toEmail.split('@')[1]
 
 #Use domain to get the proper MX server
+#split the string until only the mx server is left.
 mailServer = dns.resolver.query(domain, 'MX')[0].to_text().split(' ')[1]
 
 
@@ -43,6 +49,7 @@ class MailClient():
 
 
     #Function that sends and receives according to smtp protocol
+    #For each message sent there is a response.
     def send(self):
         self.receive()
         self.socket.send("helo\r\n") #Be classy and say hello.
@@ -55,7 +62,7 @@ class MailClient():
         self.receive()
         self.socket.send("From: <%s>\r\n"         %fromEmail
                           +"To: <%s>\r\n"         %toEmail
-                          +"Subject: %s\r\n\r\n"    %subject
+                          +"Subject: %s\r\n\r\n"  %subject
                           +"%s\r\n"               %message
                           +"\r\n.\r\n")
         self.receive()
